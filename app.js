ae947569de6ce001de45b13c58e6fc3c01afa816
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2021-10-21 22:36:37 +0100

    generated brightness and saturation for the input-range color

--- app.js
+++ app.js
@@ -14,24 +14,24 @@ function generateHex() {
     }
     return hash;
 
-    /*
-    const hexColor = chroma.random();
-    return hexColor
-    */
+    // generate color with chromajs
+    // const hexColor = chroma.random();
+    // return hexColor
 }
 
+// check text color contrast
 function checkContrast(color, text) {
     const luminance = chroma(color).luminance();
     if(luminance > 0.5) {
-        text.style.color = 'black';
+        text.style.color = '#000';
     } else {
-        text.textContent = 'white';
+        text.style.color = '#fff';
     }
 }
 
 function randomColors() {
     colorsDiv.forEach((div, index) => {
-        hexText = div.children[0];
+        const hexText = div.children[0];
         const randomColor = generateHex();
 
         // add color to background
@@ -39,11 +39,37 @@ function randomColors() {
         hexText.textContent = randomColor;
 
         // check text contrast
-        // checkContrast(randomColor, hexText);
+        checkContrast(randomColor, hexText);
 
-        // initilize colorize slider
-        // const color = chroma(randomColor);
+        // initilize colorize sliders
+        const color = chroma(randomColor);
+        const sliders = div.querySelectorAll('.sliders input');
+        const hue = sliders[0];
+        const brightness = sliders[1];
+        const saturation = sliders[2];
+
+        colorizeSlider(color, hue, brightness, saturation)
     });
+}
+
+function colorizeSlider(color, hue, brightness, saturation) {
+    // scale sturation
+    const noSaturation = color.set('hsl.s', 0);
+    const fullSaturation = color.set('hsl.s', 1);
+    const scaleSaturation = chroma.scale([noSaturation, color, fullSaturation]);
+
+    // scale Brightness
+    const midBright = color.set('hsl.l', 0.5);
+    const scaleBright = chroma.scale(['black', midBright, 'white']);
+
+    // scale hue
+
+    // update input-range colors
+
+    // saturation
+    saturation.style.background = `linear-gradient(to right, ${scaleSaturation(0)}, ${scaleSaturation(1)})`;
+    // brightness
+    brightness.style.background = `linear-gradient(to right, ${scaleBright(0)}, ${scaleBright(0.5)}, ${scaleBright(1)})`;
 }
 
 randomColors();
